109f2cc6cbe26c6e9a6dd44a43b437b29714f571
박현민 <dev.hmpark@gmail.com>
2023-12-13 21:53:02 +0900

    read

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let listData = [
+    {
+      id: "1",
+      item: "식비",
+      cost: "1200",
+    },
+    {
+      id: "2",
+      item: "밥먹기",
+      cost: "12000",
+    },
+  ];
+  const onsubmit = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="todoBlock">
+        <div className="title">
+          <h1>예산 계산기</h1>
+        </div>
+        <div>
+          <label>
+            지출 항목
+            <input
+              type="text"
+              className="input"
+              name="item"
+              placeholder="예) 렌트비"
+            />
+          </label>
+          <label>
+            비용
+            <input type="text" className="input" name="cost" defaultValue="0" />
+          </label>
+          <button className="btn" onClick={onsubmit()}>
+            제출{" "}
+          </button>
+        </div>
+        <div className="list">
+          {listData.map((data) => (
+            <div>
+              <input type="text" defaultValue={data.item} />
+              <input type="text" defaultValue={data.cost} />
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          ))}
+          <button className="btn">목록 지우기</button>
+        </div>
+      </div>
     </div>
   );
 }
